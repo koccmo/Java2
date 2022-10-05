9a81c11262186cd15a9a4f23bff3ec77109792c2
Roman <koccmo@gmail.com>
2022-10-05 12:54:08 +0300

    Create class Book

---
+++ BookLibrary/src/main/java/library/Book.java
@@ -0,0 +1,11 @@
+package library;
+
+public class Book {
+    private String name;
+    private String title;
+
+    public Book(String name, String title) {
+        this.name = name;
+        this.title = title;
+    }
+}
